a236d1e928bd241383e8fafbc750203c522406e4
Gabrielius Keibas <76430478+gabkeib@users.noreply.github.com>
2021-09-29 12:00:58 +0300

    Clear unnecessary files and make map full screen

--- WalkAndTravel/ClientApp/src/components/Map.js
+++ WalkAndTravel/ClientApp/src/components/Map.js
@@ -4,7 +4,7 @@ import RoutingMachine from "./RoutingMachine";
 
 const Map = (props) => {
     return (
-        <MapContainer center={[54.687157, 25.279652]} zoom={13} height={180} scrollWheelZoom={false}>
+        <MapContainer className = "map" center={[54.687157, 25.279652]} zoom={13} height={180} scrollWheelZoom={false}>
             <TileLayer
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
