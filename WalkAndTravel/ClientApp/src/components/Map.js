a3b08d012a30d4ac0ad1aee57d475cfcc5c7f885
Gabrielius Keibas <76430478+gabkeib@users.noreply.github.com>
2021-09-30 19:55:12 +0300

    Add Leaflet routing and clean project (#2)
    * Add routing machine into project
    
    * Split home window and map into seperate parts
    
    * Clear unnecessary files and make map full screen
    
    * Add folder for backend and remove unnecessary C# files

---
+++ WalkAndTravel/ClientApp/src/components/Map.js
@@ -0,0 +1,17 @@
+﻿import React from "react"
+import { MapContainer, TileLayer } from "react-leaflet";
+import RoutingMachine from "./RoutingMachine";
+
+const Map = (props) => {
+    return (
+        <MapContainer className = "map" center={[54.687157, 25.279652]} zoom={13} height={180} scrollWheelZoom={false}>
+            <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <RoutingMachine />
+        </MapContainer>
+    )
+}
+
+export default Map;
---
+++ WalkAndTravel/ClientApp/src/components/RoutingMachine.js
@@ -0,0 +1,27 @@
+﻿import L from "leaflet"
+import { createControlComponent } from "@react-leaflet/core";
+import "leaflet-routing-machine";
+
+const createRoutingMachineLayer = (props) => {
+    const instance = L.Routing.control({
+        waypoints: [
+            L.latLng(54.6866, 25.2865),
+            L.latLng(54.6902, 25.2764)
+        ],
+        lineOptions: {
+            styles: [{ color: "#6FA1EC", weight: 4 }]
+        },
+        show: false,
+        addWaypoints: false,
+        routeWhileDragging: true,
+        draggableWaypoints: true,
+        fitSelectedRoutes: true,
+        showAlternatives: false
+    });
+
+    return instance;
+    };
+
+    const RoutingMachines = createControlComponent(createRoutingMachineLayer);
+
+    export default RoutingMachines;
