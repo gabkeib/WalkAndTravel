51960a0a3d50fe31eb3126b57602ebee2859ec6d
Gabrielius Keibas <76430478+gabkeib@users.noreply.github.com>
2021-10-12 11:33:40 +0300

    React window implementation to represent routes

--- WalkAndTravel/ClientApp/src/components/Home.js
+++ WalkAndTravel/ClientApp/src/components/Home.js
@@ -1,8 +1,38 @@
-import React, { Component } from 'react';
+import React, { Component, memo, PureComponent } from 'react';
 import Map from "./Map";
-import SideBar from './sidebar/Sidebar';
+import Sidebar from './sidebar/Sidebar';
+import { FixedSizeList as ReactList, areEqual } from 'react-window';
 
 import './Home.css';
+
+
+/*const Row = ({ data, index, style }) => {
+    const items = data;
+    const item = items[index];
+
+    return (
+    <div className={'listItem'} style={style} >
+        {item.name} and length is {item.length}
+        </div >
+        )
+};*/
+
+class ItemRenderer extends PureComponent {
+    render() {
+        const item = this.props.data[this.props.index];
+        console.log(this.props.data);
+        return (
+            <div className={'ListItem'} style={this.props.style} onClick={() => passRoute(item)}>
+                {item.name} {this.props.index}
+            </div>
+        );
+    }
+}
+
+function passRoute(route) {
+    alert(route.name);
+}
+
 
 export class Home extends Component {
     static displayName = Home.name;
@@ -15,6 +45,7 @@ export class Home extends Component {
     componentDidMount() {
         this.populateRouteData();
     }
+   
 
     static renderRoutesTable(routes) {
         return (
@@ -37,14 +68,32 @@ export class Home extends Component {
         );
     }
 
+
+    static renderList(routes) {
+
+        return (
+            <ReactList
+                className="RList"
+                height={150}
+                itemCount={routes.length}
+                itemData={ routes}
+                itemSize={35}
+                width={300}
+            >
+                {ItemRenderer}
+            </ReactList>
+            )
+    }
+
     render() {
         let contents = this.state.loading
         ? <p><em>Loading...</em></p>
-        : Home.renderRoutesTable(this.state.routes);
+        : Home.renderList(this.state.routes);
 
         return (
-            <div>
+            <div id = "Home">
                 {contents}
+                <Sidebar />
                 <Map /> 
             </div>
         );
