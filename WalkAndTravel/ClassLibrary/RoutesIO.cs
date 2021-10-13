452b5184beb32bb244f4d854d6c42b0530657dfa
Ignas Balčiūnas <90325261+LiolikB@users.noreply.github.com>
2021-10-13 12:23:54 +0300

    Install Newtonsoft.Json and add Routes input/output class (#4)
    * Install Newtonsoft.Json and add Routes input/output class
    
    * Add Sidebar (#5)
    
    * -Sidebar
    
    * Remove overlay
    
    * Install Newtonsoft.Json and add Routes input/output class
    
    * Update routes input/output class
    
    Co-authored-by: Matas Bobin <90321426+Haiosss@users.noreply.github.com>

---
+++ WalkAndTravel/ClassLibrary/RoutesIO.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WalkAndTravel.ClassLibrary
+{
+    public class RoutesIO
+    {
+
+        /*
+         * Writes a list of routes to json file
+         */
+
+        public static void WriteRoutesToFile(List<Route> routes, string path)
+        {
+            using (var streamWriter = new StreamWriter(path))
+            {
+                var json = JsonConvert.SerializeObject(routes);
+                streamWriter.Write(json);
+            }
+        }
+
+        /*
+         * Reads from a json file and returns routes list 
+         */
+
+        public static List<Route> ReadRoutesFromFile(string path)
+        {
+            List<Route> Routes = new();
+
+            if (File.Exists(path))
+            {
+                using (var streamReader = new StreamReader(path))
+                {
+                    var json = streamReader.ReadToEnd();
+                    Routes = JsonConvert.DeserializeObject<List<Route>>(json);
+                }
+            }
+
+            return Routes;
+        }
+    }
+}
