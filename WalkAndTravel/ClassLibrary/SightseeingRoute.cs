b441c34cd6afd2bad02c88dd8d5df19e5adc7da7
Gabrielius Keibas <76430478+gabkeib@users.noreply.github.com>
2021-10-15 10:18:55 +0300

    Add sightseeing routes class

---
+++ WalkAndTravel/ClassLibrary/SightseeingRoute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WalkAndTravel.ClassLibrary
+{
+    public class SightseeingRoute : Route
+    {
+        public int DistanceFromStart
+        {
+            get;
+        }
+
+        public Marker StartingPoint
+        {
+            get; set;
+        }
+
+        public SightseeingRoute(Marker startingPoint, int distanceFromPoint)
+        {
+            StartingPoint = startingPoint;
+            DistanceFromStart = distanceFromPoint;
+        }
+    }
+}
