3763a1053e6c3509199dfb944c7d1839e9a0c938
Haiosss <matasbobin@gmail.com>
2021-10-15 09:38:57 +0300

    Added progress bar

--- WalkAndTravel/ClientApp/src/components/sidebarProfile/SidebarProfile.js
+++ WalkAndTravel/ClientApp/src/components/sidebarProfile/SidebarProfile.js
@@ -2,11 +2,17 @@
 import { slide as Menu } from 'react-burger-menu';
 import './SidebarProfile.css';
 import { BiUserCircle } from "react-icons/bi";
+import ProgressBar from 'react-bootstrap/ProgressBar'
+
+var exp = 3505;
 
 const SidebarProfile = (props) => {
     return (
         <Menu noOverlay right width={450} customBurgerIcon={<BiUserCircle />} id={"SidebarProfile"} burgerButtonClassName={"profile-sidebar-button"}>
-            
+
+            <div>
+                <ProgressBar animated min={0} max={10000} now={exp} label={`${exp} exp`} />
+            </div>
         </Menu>
     );
 };
