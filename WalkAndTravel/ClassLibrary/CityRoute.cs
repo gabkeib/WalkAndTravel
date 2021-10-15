be07bcd808673fb772897ef9fdd637e7fa7d23e7
Gabrielius Keibas <76430478+gabkeib@users.noreply.github.com>
2021-10-15 10:18:43 +0300

    Add city route with generator and add calculator related to markers

---
+++ WalkAndTravel/ClassLibrary/CityRoute.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WalkAndTravel.ClassLibrary
+{
+    public class CityRoute : Route
+    {
+
+        public int DistanceFromStart
+        {
+            get; set;
+        }
+
+        public Marker StartingPoint
+        {
+            get; set;
+        }
+
+        public int MarkersCount
+        {
+            get; set;
+        }
+
+        public CityRoute(Marker startingPoint, int distanceFromPoint, int markersCount = 3)
+        {
+            StartingPoint = startingPoint;
+            DistanceFromStart = distanceFromPoint;
+            MarkersCount = markersCount;
+        }
+
+        public void GenerateRoute()
+        {
+            var averageDistance = (double)DistanceFromStart / (double)MarkersCount;
+            var rnd = new Random();
+            var bearing = rnd.Next(30, 359) + rnd.NextDouble();
+            var current = StartingPoint;
+            var markers = new List<Marker>() { current };
+            for (int i = 0; i < MarkersCount; i++)
+            {
+                current = current.CalculateNextMarker(averageDistance, bearing);
+                markers.Add(current);
+            }
+            Markers = markers;
+        }
+    }
+}
