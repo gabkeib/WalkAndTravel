cb45607f89cd313842e05e70e7e886f9b23e3671
Gabrielius Keibas <76430478+gabkeib@users.noreply.github.com>
2021-11-08 13:39:01 +0200

    Create exception for catching illegal latitude and longitude coordinates

---
+++ WalkAndTravel/ClassLibrary/Exceptions/IllegalLatLngException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WalkAndTravel.ClassLibrary.Exceptions
+{
+    public class IllegalLatLngException : Exception
+    {
+        public IllegalLatLngException()
+        {
+
+        }
+
+        public IllegalLatLngException(string message) : base(message)
+        {
+
+        }
+
+        public IllegalLatLngException(string message, Exception inner) : base(message, inner)
+        {
+
+        }
+    }
+}
+}
