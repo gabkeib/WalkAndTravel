5f89bc9efb16301bfa05f49fa9787e039886dd4c
Gabrielius Keibas <76430478+gabkeib@users.noreply.github.com>
2021-11-08 13:38:22 +0200

    Create logging classes

---
+++ WalkAndTravel/ClassLibrary/Logging/Logger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WalkAndTravel.ClassLibrary.Logging
+{
+    public class Logger
+    {
+        public static void Log(object sender, LogEventArgs e)
+        {
+            string log = $"Date: {DateTime.Now}, Action type: {e.ActionType}, Route type: {e.RouteType}, RouteName: {e.RouteName}";
+            System.Diagnostics.Debug.WriteLine(log);
+        }
+    }
+}
