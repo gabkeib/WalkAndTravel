7b96e88f716290835dc37a15c3e11f4415872aa5
LiolikB <balcignas@gmail.com>
2021-11-17 20:18:14 +0200

    Create services

---
+++ WalkAndTravel/ClassLibrary/Services/IRouteServices.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WalkAndTravel.ClassLibrary.Services
+{
+    public interface IRouteServices
+    {
+        Task<IEnumerable<Route>> GetRoutes();
+
+        Route GetRandomRoute();
+
+        Route GetRandomPOIRoute();
+
+        int SaveNewRoute(RouteMinimal routes);
+    }
+}
---
+++ WalkAndTravel/ClassLibrary/Services/IUserServices.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WalkAndTravel.Models;
+
+namespace WalkAndTravel.ClassLibrary.Services
+{
+    public interface IUserServices
+    {
+        Task<IEnumerable<User>> GetUsers();
+
+        int CreateNewUser(User user);
+
+        Task<User> GetByEmail(string email);
+
+        Task<User> GetById(int id);
+    }
+}
