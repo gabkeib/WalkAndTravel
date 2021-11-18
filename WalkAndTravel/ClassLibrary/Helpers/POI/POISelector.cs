3d6eea23bd5bc0dd7ebba02125b21fd7a9689585
Jonas <jonas.dovidaitis01@gmail.com>
2021-11-18 17:16:45 +0200

    Add missing files (they were ignored in previous commit).

---
+++ WalkAndTravel/ClassLibrary/Helpers/POI/POISelector.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WalkAndTravel.ClassLibrary
+{
+    public class POISelector : IPOISelector
+    {
+        private Lazy<POIData> _data;
+        public POISelector()
+        {
+            _data = new Lazy<POIData>(() =>
+            {
+                return new POIData();
+            });
+        }
+
+        public POIData Data
+        {
+            get
+            {
+                return _data.Value;
+            }
+        }
+        public POIList SelectPOI(Amenity amenity = Amenity.Undefined)
+        {
+            var poiData = new POIList();
+            if (amenity.Equals(Amenity.Undefined))
+            {
+                var index = 0;
+                foreach (var poi in Data)
+                {
+                    poiData[index] = (POI) poi;
+                    index++;
+                    if (index >= poiData.Length)
+                    {
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                var filteredData =
+                    from poiOb in Data
+                    where poiOb.Amenity == amenity
+                    select poiOb;
+
+                var index = 0;
+                foreach (var poi in filteredData)
+                {
+                    poiData[index] = (POI) poi;
+                    index++;
+                    if (index >= poiData.Length)
+                    {
+                        break;
+                    }
+                }
+            }
+            return poiData;
+        }
+    }
+}
---
+++ WalkAndTravel/ClassLibrary/Models/CityRoute.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WalkAndTravel.ClassLibrary
+{
+    public class CityRoute : Route
+    {
+        public bool GetToStart
+        {
+            get; set;
+        }
+        public int DistanceFromStart
+        {
+            get; set;
+        }
+
+        public Marker StartingPoint
+        {
+            get; set;
+        }
+
+        public int MarkersCount
+        {
+            get; set;
+        }
+
+        public CityRoute(Marker startingPoint, int distanceFromPoint, int markersCount = 4, bool getToStart = true)
+        {
+            StartingPoint = startingPoint;
+            DistanceFromStart = distanceFromPoint;
+            MarkersCount = markersCount;
+            GetToStart = getToStart;
+        }
+
+        public void GenerateRoute(Func<double> bearingCalclulator)
+        {
+            var averageDistance = (double)DistanceFromStart / MarkersCount;
+            var rnd = new Random();
+            var bearing = rnd.Next(30, 359) + rnd.NextDouble();
+            var current = StartingPoint;
+            var markers = new List<Marker>() { current };
+            for (int i = 0; i < MarkersCount; i++)
+            {
+                try
+                {
+                    current = current.CalculateNextMarker(averageDistance, bearing);
+                } catch (Exceptions.IllegalLatLngException ex)
+                {
+                    System.Diagnostics.Debug.WriteLine(ex);
+                    current = StartingPoint;
+                }
+                markers.Add(current);
+                bearing = bearingCalclulator();
+            }
+            if (GetToStart)
+            {
+                markers.Add(StartingPoint);
+            }
+            Markers = markers;
+        }
+    }
+}
