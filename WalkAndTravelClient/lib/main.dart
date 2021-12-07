6b332af796c20c14ab3d529979215a96d556de5a
Jonas <jonas.dovidaitis01@gmail.com>
2021-12-07 03:42:07 +0200

    Add two pages: Registration and Login. Not fully completed yet, but more than a halfway there.

--- WalkAndTravelClient/lib/main.dart
+++ WalkAndTravelClient/lib/main.dart
@@ -5,6 +5,8 @@ import 'package:flutter/material.dart';
 
 import 'package:latlong2/latlong.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
+import 'package:walk_and_travel/pages/login_page.dart';
+import 'package:walk_and_travel/pages/registration_page.dart';
 import 'package:walk_and_travel/widgets/osmmap.dart';
 import 'models/route.dart' as a;
 import 'models/minimal_route.dart' as b;
@@ -23,17 +25,13 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      routes: {
+        '/' : (context) => const LoginPage(),
+        '/register' : (context) => const RegisterPage(),
+      },
+      //home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
---
+++ WalkAndTravelClient/lib/pages/login_page.dart
@@ -0,0 +1,197 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:snippet_coder_utils/FormHelper.dart';
+import 'package:snippet_coder_utils/ProgressHUD.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool isAPICallProcess = false;
+  bool hidePassword = true;
+  GlobalKey<FormState> globalFormKey = GlobalKey<FormState>();
+  String? email;
+  String? password;
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.indigoAccent,
+        body: ProgressHUD(
+          child: Form(
+            key: globalFormKey,
+            child: _loginUI(context),
+          ),
+          inAsyncCall: isAPICallProcess,
+          opacity: 0.3,
+          key: UniqueKey(),
+        ),
+      ),
+    );
+  }
+
+  Widget _loginUI(BuildContext context){
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height/6,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Colors.white,
+                  Colors.greenAccent
+                ],
+              ),
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(100),
+                bottomRight: Radius.circular(100)
+              ),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Align(
+                  alignment: Alignment.center,
+                  child: Image.asset(
+                    "assets/images/WATLogin2.png",
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height/6,
+                    fit: BoxFit.contain
+                  ),
+                )
+              ]
+            )
+          ),
+          const Padding(
+            padding: EdgeInsets.only(
+              left: 20,
+              bottom: 30,
+              top: 75
+            ),
+            child: Text(
+              "Login",
+              style:TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 25,
+                color: Colors.white
+              ),
+            ),
+          ),
+          FormHelper.inputFieldWidget(
+              context,
+              const Icon(Icons.person),
+              "email",
+              "Email",
+              (onValidateVal){
+                if(onValidateVal.isEmpty){
+                  return "Email can't be empty.";
+                }
+                return null;
+              },
+              (onSavedVal){
+                email = onSavedVal;
+              },
+            borderFocusColor: Colors.white,
+            prefixIconColor: Colors.white,
+            borderColor: Colors.white,
+            textColor: Colors.white,
+            hintColor: Colors.white.withOpacity(0.7),
+            borderRadius: 10,
+          ),
+          Padding(
+              padding: const EdgeInsets.only(top: 10),
+              child: FormHelper.inputFieldWidget(
+                context,
+                const Icon(Icons.person),
+                "password",
+                "Password",
+                    (onValidateVal){
+                  if(onValidateVal.isEmpty){
+                    return "Password can't be empty.";
+                  }
+                  return null;
+                },
+                    (onSavedVal){
+                  password = onSavedVal;
+                },
+                borderFocusColor: Colors.white,
+                prefixIconColor: Colors.white,
+                borderColor: Colors.white,
+                textColor: Colors.white,
+                hintColor: Colors.white.withOpacity(0.7),
+                borderRadius: 10,
+                obscureText: hidePassword,
+                suffixIcon: IconButton(
+                    onPressed: (){
+                      setState(() {
+                        hidePassword = !hidePassword;
+                      });
+                    },
+                    color: Colors.white.withOpacity(0.7),
+                    icon: Icon(
+                      hidePassword ?  Icons.visibility_off : Icons.visibility
+                    ),
+                ),
+              ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Center(
+              child: FormHelper.submitButton(
+                "Login",
+                    (){},
+                btnColor: Colors.blueAccent,
+                borderColor: Colors.white,
+                txtColor: Colors.white,
+                borderRadius: 10,
+              )
+          ),
+          const SizedBox(height: 20,),
+
+          Align(
+            alignment: Alignment.center,
+            child: Padding(
+              padding: const EdgeInsets.only(right:25, top: 10),
+              child: RichText(
+                text: TextSpan(
+                    style: const TextStyle(
+                        color: Colors.grey,
+                        fontSize: 14
+                    ),
+                    children: <TextSpan>[
+                      const TextSpan(text: "Don't have an account?  "),
+                      TextSpan(
+                        text: 'Sign up',
+                        style: const TextStyle(
+                          color: Colors.white,
+                          decoration: TextDecoration.underline,
+                        ),
+                        recognizer: TapGestureRecognizer()..onTap = (){
+                          Navigator.pushNamed(context, "/register");
+                        },
+                      ),
+                    ]
+                )
+              )
+            ),
+          )
+
+        ],
+
+      )
+    );
+  }
+}
