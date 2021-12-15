f240b6f81d840010c3a70e2e44c06a2bee8fa99a
Gabrielius Keibas <76430478+gabkeib@users.noreply.github.com>
2021-12-15 17:45:56 +0200

    Change Route test folder

--- WalkAndTravelTests/Repository/RouteRepositoryTestWindow.cs
+++ WalkAndTravelTests/Repository/RouteRepositoryTestWindow.cs
@@ -9,10 +9,15 @@ namespace WalkAndTravelTests.Controllers
 {
     public class RouteRepositoryTestWindow
     {
+        private SqliteRouteRepositoryTest sqliteRouteRepositoryTest;
+        public RouteRepositoryTestWindow()
+        {
+            sqliteRouteRepositoryTest = new SqliteRouteRepositoryTest();
+        }
+
         [Fact]
         public void RouteRepository_GetRoutes_ReturnsCorrectNumberOfRoutes()
         {
-            SqliteRouteRepositoryTest sqliteRouteRepositoryTest = new SqliteRouteRepositoryTest();
             sqliteRouteRepositoryTest.RouteRepository_GetRoutes_ReturnsCorrectNumberOfRoutes();
         }
 
@@ -22,41 +27,35 @@ namespace WalkAndTravelTests.Controllers
         [InlineData(0, 2)]
         public void RouteRepository_GetPagingRouteList_ReturnsCorrectNumberOfRoutes(int page, int pageSize)
         {
-            SqliteRouteRepositoryTest sqliteRouteRepositoryTest = new SqliteRouteRepositoryTest();
             sqliteRouteRepositoryTest.RouteRepository_GetPagingRouteList_ReturnsCorrectNumberOfRoutes(page, pageSize);
         }
 
         [Fact]
         public void RouteRepository_SaveNewRoute_InsertsUserCorrectly()
         {
-            SqliteRouteRepositoryTest sqliteRouteRepositoryTest = new SqliteRouteRepositoryTest();
             sqliteRouteRepositoryTest.RouteRepository_SaveNewRoute_InsertsUserCorrectly();
         }
         
         [Fact]
         public void RouteRepository_DeleteRoute_DeletesCorrectRoute()
         {
-            SqliteRouteRepositoryTest sqliteRouteRepositoryTest = new SqliteRouteRepositoryTest();
             sqliteRouteRepositoryTest.RouteRepository_DeleteRoute_DeletesCorrectRoute();
         }
 
         [Fact]
         public void RouteRepository_GetRandomRoute_ReturnsCorrectRoute()
         {
-            SqliteRouteRepositoryTest sqliteRouteRepositoryTest = new SqliteRouteRepositoryTest();
             sqliteRouteRepositoryTest.RouteRepository_GetRandomRoute_ReturnsCorrectRoute();
         }
         [Fact]
         public void RouteRepository_GetRandomPOIRoute_ReturnsCorrectRoute()
         {
-            SqliteRouteRepositoryTest sqliteRouteRepositoryTest = new SqliteRouteRepositoryTest();
             sqliteRouteRepositoryTest.RouteRepository_GetRandomPOIRoute_ReturnsCorrectRoute();
         }
 
         [Fact]
         public void RouteRepository_GetRoutesNumbers_ReturnsCorrectRouteStatistics()
         {
-            SqliteRouteRepositoryTest sqliteRouteRepositoryTest = new SqliteRouteRepositoryTest();
             sqliteRouteRepositoryTest.RouteRepository_GetRoutesNumbers_ReturnsCorrectRouteStatistics();
         }
     }
