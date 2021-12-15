eb6662be6115e1240265aa90f34cac44811fa484
Gabrielius Keibas <76430478+gabkeib@users.noreply.github.com>
2021-12-15 17:45:25 +0200

    Add User tests

---
+++ WalkAndTravelTests/Repository/UserRepositoryTestWindow.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WalkAndTravelTests.Controllers;
+using Xunit;
+
+namespace WalkAndTravelTests.Repository
+{
+    public class UserRepositoryTestWindow
+    {
+        private SqliteUserRepositoryTest _sqliteUserRepositoryTest;
+        public UserRepositoryTestWindow()
+        {
+            _sqliteUserRepositoryTest = new SqliteUserRepositoryTest();
+        }
+
+        [Fact]
+        public void UserRepository_GetUsers_ReturnsCorrectAmountOfUsers()
+        {
+            _sqliteUserRepositoryTest.UserRepository_GetUsers_ReturnsCorrectAmountOfUsers();
+        }
+
+        [Theory]
+        [InlineData ("badmail@two.com", "Jurgita")]
+        [InlineData ("ech@outbox.com", "Jin")]
+        public void UserRepository_GetByEmail_ReturnsCorrectUser(string email, string name)
+        {
+            _sqliteUserRepositoryTest.UserRepository_GetByEmail_ReturnsCorrectUser(email, name);
+        }
+
+        [Theory]
+        [InlineData(2, "Jurgita")]
+        [InlineData(3, "Jin")]
+        public void UserRepository_GetById_ReturnsCorrectUser(int id, string name)
+        {
+            _sqliteUserRepositoryTest.UserRepository_GetById_ReturnsCorrectUser(id, name);
+        }
+
+        [Theory]
+        [InlineData(1, "Kazimeras")]
+        [InlineData(3, "Jin")]
+        public void UserRepository_DeleteUser_DeletesCorrectUser(int id, string name)
+        {
+            _sqliteUserRepositoryTest.UserRepository_DeleteUser_DeletesCorrectUser(id, name);
+        }
+
+        [Fact]
+        public void UserRepository_CreateNewUser_AddsCorrectUser()
+        {
+            _sqliteUserRepositoryTest.UserRepository_CreateNewUser_AddsCorrectUser();
+        }
+    }
+}
+
